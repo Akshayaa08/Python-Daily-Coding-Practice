ae1b6abede706d3e0f47ba90055d55e1cf685d96
Akshayaa08 <akshayaa.26it@licet.ac.in>
2026-05-18 22:20:07 +0530

    Added Exercise and removed comment line

--- Input.py
+++ Input.py
@@ -3,24 +3,24 @@
 # Returns the entered data as a string
 
 
-# name = input("Enter your name:")
-# age =input("Enter your age:")
-# #the age is collected as a string, we need to convert it to an integer to perform arithmetic operations on it.
-# age =int(age)
-# age = age+1
-# age =int(input("Enter your age:"))
-# print(f"Hello {name}, you are {age} years old")
+name = input("Enter your name:")
+age =input("Enter your age:")
+#the age is collected as a string, we need to convert it to an integer to perform arithmetic operations on it.
+age =int(age)
+age = age+1
+age =int(input("Enter your age:"))
+print(f"Hello {name}, you are {age} years old")
 
-# #Exercise 1 : Rectangle Area Calculator
-# Length = int(input("Enter the length of the rectangle:"))
-# Width = int(input("Enter the width of the rectangle:"))
-# area = int(Length)*int(Width)
-# print (area)
+#Exercise 1 : Rectangle Area Calculator
+Length = int(input("Enter the length of the rectangle:"))
+Width = int(input("Enter the width of the rectangle:"))
+area = int(Length)*int(Width)
+print (area)
 
 
 #Exercise 2 : Shopping  Cart Program
 item = input ("Enter the item you want to buy:")
-price = float(input(" Enter the price of the items:"))
+price = float(input("Enter the price of the items:"))
 quantity = int(input("Enter the quantity of the items:"))
 total = price * quantity
 print(f"Total cost for {quantity} {item}(s): ${total:.2f}")
