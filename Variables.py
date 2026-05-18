6f2dd58a921ef265075e0b977f41d0c160ea0db6
Akshayaa08 <akshayaa.26it@licet.ac.in>
2026-05-18 20:18:36 +0530

    Feat: Assigning variables

---
+++ Variables.py
@@ -0,0 +1,23 @@
+# Variable = A container of values to store different tpyes of data
+# Such as integer, float, string, boolean, list, tuple, set, dictionary
+
+
+#Strings is a series of character they can include numbers and symbols but are treated as character inside the quotation of the string.
+#Assigning variables for a Strings 
+First_name = "Gino"
+food ="Pizza"
+email ="gino@example.com"
+
+print(f"My name is {First_name}")
+print (f"I like {food}")
+print(f"My email addresss is {email}")
+
+#Integer is a whole number that can be positive, negative or zero. They do not have decimal points.
+#Assigning variables for anInteger 
+
+age = 23
+quantity = 3
+tot_students = 60
+
+print(f"You are {age} years old")
+print(f"")
