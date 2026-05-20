10c77de86bd7249b988bb3b628bce2ac9f00d725
Akshayaa08 <akshayaa.26it@licet.ac.in>
2026-05-20 22:10:17 +0530

    IF STATEMENT

---
+++ If_Statement.py
@@ -0,0 +1,42 @@
+# # if = Do some code only if some condition is true 
+# # else do is when the if statement is false this statement gets executed
+
+# age = int (input("Enter your age: "))
+
+# if age>=18:
+#     print("You are now signed up!")
+# elif age<0 :
+#     print("You haven't been born yet!")
+# elif age>=100:
+#     print("You are too old to sign up")
+# else:
+#     print("You must be 18+ to sign up")
+
+# response = input("Would you like food? (Y/N): ")
+
+# if response == "Y":
+#     print("Have some food!")
+# elif response =="N":
+#     print("Don't want any food!")
+# else:
+#     print("Invalid input")
+
+# name = input("Enter your name: ")
+
+# if name == "":
+#     print("You did not type in your name!")
+# else:
+#     print(f"Hello {name}")
+
+for_sale = True 
+if for_sale:
+    print("This item is for sale")
+else:
+    print("This item is NOT for sale")
+
+
+Online = False 
+if Online:
+    print("This user is online")
+else:
+    print("This user is offline")
