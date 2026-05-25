751940655c3649e46b69427c52d26927b7d71566
Akshayaa08 <akshayaa.26it@licet.ac.in>
2026-05-25 23:39:34 +0530

    Shopping Cart bill for foods

---
+++ 21.Shopping_Cart.py
@@ -0,0 +1,25 @@
+# Shopping Cart exercise
+
+foods =[]
+prices = [] #list ordered and changeable
+total = 0
+
+while True:
+    food=input("Enter the food (q to quit): ")
+    if food.lower()=="q":
+        break
+    else:
+        price = float(input(f"Enter the price of a food {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("-----YOUR CART------")
+
+for food in foods:
+    print(food, end =" ")
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your total amount is {total}")
