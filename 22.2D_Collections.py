53acc660ec309246bc2f28b46c4284f91724616b
Akshayaa08 <akshayaa.26it@licet.ac.in>
2026-05-26 22:50:27 +0530

    2 dimensional collections

---
+++ 22.2D_Collections.py
@@ -0,0 +1,36 @@
+# # 2D Collection
+
+# # fruits    = ["apple","banana","orange","pineapple"]
+# # vegetable = ["carrot","brinjal","tomato"]
+# # meat      = ["fish","chicken","mutton"]
+
+# # groceries = [fruits,vegetable,meat]
+
+# # print(groceries)
+# # print(groceries[0])
+# # print(groceries[0][0])
+
+# groceries = [["apple","banana","orange","pineapple"],
+#              ["carrot","brinjal","tomato"],
+#              ["fish","chicken","mutton"]]
+# # print(groceries)
+# # print(groceries[0])
+# # print(groceries[0][1])
+
+
+# for collection in groceries:
+#     for food in collection:
+#         print(food, end =" ")
+#     print()
+
+
+num_pad = ((1,2,3),
+           (4,5,6),
+           (7,8,9),
+           ("*",0,"#"))
+
+for rows in num_pad:
+    for num in rows:
+        print(num, end=" ")
+    print()
+
