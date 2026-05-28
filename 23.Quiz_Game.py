f3d6db7859ac96842ee54b1f54c1ea6e60afcc75
Akshayaa08 <akshayaa.26it@licet.ac.in>
2026-05-28 23:54:30 +0530

    Quiz Games using Collections

---
+++ 23.Quiz_Game.py
@@ -0,0 +1,20 @@
+#Python_Quiz_Game
+
+
+questions = ("How many elements in a periodic table?: ",
+             "Which animals lay the largest egg?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are in the human body?: ",
+             "Which planet in the solar system is the hottest?: ")
+
+options= (("A. 116","B. 117","C. 118","D. 119"),
+          ("A.Ostrich ", "B. ","C. ", "D. "),
+          ("A. ", "B. ","C. ", "D. "),
+          ("A. ", "B. ","C. ", "D. "),
+          ("A. ", "B. ","C. ", "D. "))
+
+answers = ()
+guesses = []
+score = 0
+question_num = 0
+
