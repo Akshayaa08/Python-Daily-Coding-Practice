3e4dd0d7f177b6efb7a1aafff0284bdb9f63c36c
Akshayaa08 <akshayaa.26it@licet.ac.in>
2026-05-30 21:21:52 +0530

    Dictionary is a key value pair

---
+++ 24.Dictionaries.py
@@ -0,0 +1,11 @@
+# Dictionary = a collection of {key : value} pairs
+#           ordered and changeable. No Duplicates
+
+capitals = {"USA":"Washington D.C",
+            "India":"New Delhi",
+            "China" : "Beijing",
+            "Russia" : "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+print(capitals.get("India"))
